d82645d520e505095dad7842028bf961b68af224
azevedorar <lucas.timmy1@gmail.com>
2024-09-18 18:36:37 -0300

    test

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Sidebar } from "./Components/Sidebar"
+function App() {
+  
+
+  return (
+    <>
+      <div className="flex mx-auto max-w-7xl">
+       <Sidebar/>
+      </div>
+      <main>
+        
+      </main>
+    </>
+  )
+}
+
+export default App
