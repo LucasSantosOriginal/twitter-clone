c5f34dd9585480a9dd8c8788856cd2add98151e7
azevedorar <lucas.timmy1@gmail.com>
2024-09-22 23:59:15 -0300

    atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,57 @@
 import { Sidebar } from "./Components/Sidebar"
+import { Tweet } from "./Components/Tweet"
+import { TwitterForm } from "./Components/TwitterForm"
+import { v4 } from "uuid";
+import { getAvatar, getRandomImage } from "./utils/generateImages";
+import { useState,useEffect } from "react";
+
+
 function App() {
-  
+const [tweets, setTweets] = useState([])
+
+
+
+const addNewTweet = (content,incluideImage = false) => {
+  const newTweet = {
+    id: v4(),
+    name:"User",
+    username: 'user${Math.floor(Math.random() * 1000)}',
+    avatar: getAvatar('user${Math.floor(Math.random() * 1000)}@email.com'),
+    content,
+    time: new Date().toLocaleString ([],{
+      hour:'2-digit',
+      minute:'2-digit'
+    }),
+    Image: incluideImage ? getRandomImage() : null,
+    likes: 0,
+    retweets:0,
+    comments:0
+
+  }
+  setTweets ((prevTweets)=> [newTweet, ...prevTweets])
+
+
+
+}
 
   return (
-    <>
+   
       <div className="flex mx-auto max-w-7xl">
        <Sidebar/>
-      </div>
-      <main>
-        
+     
+      <main className="flex-grow border-l border-r border-gray-700 max-w-xl">
+        <header className="sticky top-0 z-10 bg-twitter-background bg-opacity-80 backdrop-blur-sm">
+          <h2 className="px-4 py-3 text-xl font-bold">For You</h2>
+        </header>
+        <TwitterForm onTweet={(content) => addNewTweet(content, Math.random() > 0.6)}/>
+        <div>
+          {tweets.map(tweet => (
+            <Tweet key={tweet.id}  tweet={tweet}/>
+          ))}
+         
+        </div>
       </main>
-    </>
+      </div>
   )
 }
 
---
+++ src/Components/TwitterForm/index.jsx
@@ -0,0 +1,41 @@
+import { text } from "@fortawesome/fontawesome-svg-core";
+import { faCalendarAlt, faChartBar, faFilm, faImage, faMapMarked, faSmile } from "@fortawesome/free-solid-svg-icons";
+import { faMapMarkedAlt } from "@fortawesome/free-solid-svg-icons/faMapMarkedAlt";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRef } from 'react';
+
+export function TwitterForm ({onTweet}){
+const textAreaRef = useRef ()
+
+
+    function handleSubmit() {
+
+        if (textAreaRef.current.value) {
+            onTweet (textAreaRef.current.value)
+            textAreaRef.current.value = ''
+        }
+        
+    }
+    return(
+        <div className="border-b border-gray-800 p-4">
+            <textarea
+            className="w-full bg-transparent text-white text-wl resize-none outline-none"
+            placeholder="QQ TA PEGANDO VEINHO?"
+            ref={textAreaRef}
+             />
+             <div className="flex justify-between items-center mt4">
+                <div className="flex space-x-4">
+                    <FontAwesomeIcon icon={faImage} className="text-blue-400 cursor-pointer"/>
+                    <FontAwesomeIcon icon={faFilm} className="text-blue-400 cursor-pointer"/>
+                    <FontAwesomeIcon icon={faChartBar} className="text-blue-400 cursor-pointer"/>
+                    <FontAwesomeIcon icon={faSmile} className="text-blue-400 cursor-pointer"/>
+                    <FontAwesomeIcon icon={faCalendarAlt} className="text-blue-400 cursor-pointer"/>
+                    <FontAwesomeIcon icon={faMapMarkedAlt} className="text-blue-400 cursor-pointer"/>
+    
+                </div>
+                <button className="bg-blue-400 text-white font-bold px-4 py-2 rounded-full hover:bg-blue-600 transition duration-200" onClick={handleSubmit}>Tweet</button>
+             </div>
+        </div>
+
+    )
+}
